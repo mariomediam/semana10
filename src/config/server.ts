0bf0b9c59448e64fee080f301da8f17f2e6c0285
mariomediam <54757091+mariomediam@users.noreply.github.com>
2021-10-20 20:44:51 -0500

    chore: se culmina configuracion inicial

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,10 +1,18 @@
-import express, { Express } from "express";
+import express, { Express, json } from "express";
 
 export default class Server {
   private readonly app: Express;
   private readonly port: string;
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT ?? "8000";
+  }
+  bodyParser() {
+    this.app.use(json());
+  }
+  start() {
+    this.app.listen(this.port, () => {
+      console.log(`Servidor corriendo en el puerto ${this.port} 👍👍👍`);
+    });
   }
 }
