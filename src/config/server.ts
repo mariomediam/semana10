ac644ba20ab7ce17ebaa03f280a1273ce51d1057
mariomediam <54757091+mariomediam@users.noreply.github.com>
2021-10-20 21:29:29 -0500

    chore: se agrega archivo .env

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,4 +1,5 @@
 import express, { Express, json } from "express";
+import { connect } from "mongoose";
 
 export default class Server {
   private readonly app: Express;
@@ -12,8 +13,15 @@ export default class Server {
     this.app.use(json());
   }
   start() {
-    this.app.listen(this.port, () => {
+    this.app.listen(this.port, async () => {
       console.log(`Servidor corriendo en el puerto ${this.port} 👍👍👍`);
+      try {
+        console.log(process.env.DATABASE_URL)     
+          await connect(process.env.DATABASE_URL ?? "")
+          console.log("Servidorde BD conetctado exitosamente")
+      } catch (error) {
+          console.log("Error al conectar la BD")
+      }
     });
   }
 }
